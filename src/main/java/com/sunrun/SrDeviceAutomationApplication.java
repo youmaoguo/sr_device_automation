6ccce1928a6fed6d0aa3b20f945eff28b0d198d3
jet <1481205121@qq.com>
2017-06-08 14:31:05 +0800

    1

--- src/main/java/com/sunrun/SrDeviceAutomationApplication.java
+++ src/main/java/com/sunrun/SrDeviceAutomationApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
 @EnableScheduling
 @SpringBootApplication
 public class SrDeviceAutomationApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SrDeviceAutomationApplication.class, args);
 	}
+	
 }
--- src/test/java/com/sunrun/SrDeviceAutomationApplicationTests.java
+++ src/test/java/com/sunrun/SrDeviceAutomationApplicationTests.java
@@ -1,19 +1,30 @@
 package com.sunrun;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.sunrun.entity.DevOnlineTask;
+import com.sunrun.service.DeviceAutomationService;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class SrDeviceAutomationApplicationTests {
 
-	
+	@Resource
+	private DeviceAutomationService deviceAutomationService;
 
 	@Test
 	public void contextLoads() {
-		
+
+		System.out.println(11);
+		List<DevOnlineTask> li = deviceAutomationService.findPort("1");
+		System.out.println(li.size());
 	}
 
 	
